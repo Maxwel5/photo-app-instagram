ecb46d0d411a4057f66f932fcec8e25ebfe18293
Maxwel5 <maxwellwafulasim@gmail.com>
2019-10-15 13:23:29 +0300

    Upgrading settings

--- instagram/views.py
+++ instagram/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
-
 # Create your views here.
-# def instagram(request):
-#     return render(request,'instagram.html')
 
 def image(request):
-    image = Image.objects.all()
-    return render(request, instas/image.html)
+    return render(request, 'instas/image.html')
