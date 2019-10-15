b8efa1143eafc0403d3a404272cc1e1f754a5eb9
Maxwel5 <maxwellwafulasim@gmail.com>
2019-10-15 15:01:17 +0300

    Saving validated form values inside cleaned data followed by name and email retrival from the form andcreate instagram form objects

--- instagram/views.py
+++ instagram/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http  import HttpResponse
+from django.http  import HttpResponse,HttpResponseRedirect
 from .models import Image, Profile
 from .forms import InstagramForm
 # Create your views here.
@@ -10,7 +10,11 @@ def image(request):
     if request.method == 'POST':
         form = InstagramForm(request.POST)
         if form.is_valid():
-            print('valid')
+            name = form.cleaned_data['your_name']
+            email = form.cleaned_data['email']
+            recipient = InstagramForm(name = name,email =email)
+            recipient.save()
+            HttpResponseRedirect('image')
     else:
         form = InstagramForm()
     return render(request, 'instas/image.html', {"images":images,"letterForm":form})
