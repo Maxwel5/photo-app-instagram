6f4d8c195c7c6c17c1b073061afd98096e9790e0
Maxwel5 <maxwellwafulasim@gmail.com>
2019-10-15 12:19:24 +0300

    Defining an image to get all the images

--- instagram/views.py
+++ instagram/views.py
@@ -2,10 +2,9 @@ from django.shortcuts import render
 from django.http  import HttpResponse
 
 # Create your views here.
-def instagram(request):
-    # return HttpResponse('Instagram App')
-    return render(request,'instagram.html')
+# def instagram(request):
+#     return render(request,'instagram.html')
 
 def image(request):
-    # image = 
+    image = Image.objects.all()
     return render(request, instas/image.html)
