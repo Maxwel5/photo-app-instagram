cdd803311571ad3c52ea5624635e7d51b6bfdaf7
Maxwel5 <maxwellwafulasim@gmail.com>
2019-10-15 09:25:07 +0300

    Import render from django shortcuts and return a render function and pass in the request and the template file

--- instagram/views.py
+++ instagram/views.py
@@ -1,6 +1,7 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 from django.http  import HttpResponse
 
 # Create your views here.
 def instagram(request):
-    return HttpResponse('Instagram App')
+    # return HttpResponse('Instagram App')
+    return render(request,'instagram.html')
