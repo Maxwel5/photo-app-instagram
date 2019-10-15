7f1b7ce03d53926b6e3780e434c9bb4101bbb0ac
Maxwel5 <maxwellwafulasim@gmail.com>
2019-10-15 17:35:15 +0300

    Importing the from send_email_and call it after validating the form passing the name and email of the subscriber to the instagram to receive the message

--- instagram/views.py
+++ instagram/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http  import HttpResponse,HttpResponseRedirect
 from .models import Image, Profile,Instagram
 from .forms import InstagramForm
+from .email import send_welcome_email
 # Create your views here.
 
 def image(request):
@@ -17,4 +18,19 @@ def image(request):
             HttpResponseRedirect('image')
     else:
         form = InstagramForm()
+    return render(request, 'instas/image.html', {"images":images,"letterForm":form})
+
+def news_today(request):
+    if request.method == 'POST':
+        form = InstagramForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['your_name']
+            email = form.cleaned_data['email']
+
+            recipient = Instagram(name = name,email =email)
+            recipient.save()
+            send_welcome_email(name,email)
+
+            HttpResponseRedirect('image')
+            #.................
     return render(request, 'instas/image.html', {"images":images,"letterForm":form})
