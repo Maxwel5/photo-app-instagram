e4146bb1fc709788d1e77aa156a712eac8ec98d4
Maxwel5 <maxwellwafulasim@gmail.com>
2019-10-15 14:42:39 +0300

    Import instagram form class for the form to submit sensitve data to the database, also checking the post request if the form is valid and if not return an empty form instance passed in the template

--- instagram/views.py
+++ instagram/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
 from .models import Image, Profile
+from .forms import InstagramForm
 # Create your views here.
 
 def image(request):
     images = Image.objects.all()
-    return render(request, 'instas/image.html', {"images":images})
+
+    if request.method == 'POST':
+        form = InstagramForm(request.POST)
+        if form.is_valid():
+            print('valid')
+    else:
+        form = InstagramForm()
+    return render(request, 'instas/image.html', {"images":images,"letterForm":form})
