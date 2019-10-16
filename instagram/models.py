499ad7c469ed784f51f5b14582c8d9d1f98b7fe2
Maxwel5 <maxwellwafulasim@gmail.com>
2019-10-16 14:34:04 +0300

    Improvise a search button in the navbar

--- instagram/models.py
+++ instagram/models.py
@@ -47,10 +47,6 @@ class Image(models.Model):
         instagram.objects.all().delete()
         Image.objects.all().delete()
 
-    def test_get_image(self):
-        image = Image.image()
-        self.assertTrue(len(image)>0)
-
 class Instagram(models.Model):
     name = models.CharField(max_length = 20)
     email = models.EmailField()
