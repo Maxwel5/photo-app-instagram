7fa5d5305ed5da609879ca5a2fdcf3f529e20d4a
Maxwel5 <maxwellwafulasim@gmail.com>
2019-10-16 10:33:20 +0300

    Aname returning function of the string and adding an auto date field for publication

--- instagram/models.py
+++ instagram/models.py
@@ -18,6 +18,10 @@ class Image(models.Model):
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
     like_count = models.IntegerField(default=0)
     comments = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
 
 class Instagram(models.Model):
     name = models.CharField(max_length = 20)
